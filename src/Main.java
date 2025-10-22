a569260139753f1bb76ac3cb86359476f7626de3
ILisiv <ilisiv.volkov01@gmail.com>
2025-10-22 20:30:37 +0200

    first changes

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public void main() {
+
+    var hello   = "Hello Vistula";
+    IO.println(hello);
+
+    for (int i = 1; i <= 10; i++) {
+        if (i % 2 == 0)
+        {
+            IO.println(i);
+        }
+        else
+        {
+
+        }
+
+    }
+}
