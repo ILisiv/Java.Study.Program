18190229af0800da047800249b736edb8d049087
ILisiv <ilisiv.volkov01@gmail.com>
2025-10-23 00:03:37 +0200

    Task 3 names

--- src/LabProject2Task3.java
+++ src/LabProject2Task3.java
@@ -1,14 +1,11 @@
 
 public void main()
 {
-    // Create Random object
     Random rand = new Random();
 
-    // Generate a random number between 1 and 100 (inclusive)
     int randomNumber = rand.nextInt(100) + 1;
     System.out.println("Random number between 1 and 100: " + randomNumber);
 
-    // Check if the number is odd or even using modulo
     if (randomNumber % 2 == 0) {
         System.out.println(randomNumber + " is even.");
     } else {
