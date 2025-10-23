36fd77aab7b4d096670a6aa9a95a46f3af3342aa
ILisiv <ilisiv.volkov01@gmail.com>
2025-10-23 09:29:05 +0200

    test

--- src/LabProject2Task4.java
+++ src/LabProject2Task4.java
@@ -3,12 +3,5 @@ public void main()
 {
     Random rand = new Random();
 
-    int randomNumber = rand.nextInt(100) + 1;
-    System.out.println("Random number between 1 and 100: " + randomNumber);
 
-    if (randomNumber % 2 == 0) {
-        System.out.println(randomNumber + " is even.");
-    } else {
-        System.out.println(randomNumber + " is odd.");
-    }
 }
