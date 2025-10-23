950a7e6255924a60a55f2ad3847df2ae3b48dbec
ILisiv <ilisiv.volkov01@gmail.com>
2025-10-23 17:03:45 +0200

    asdasd

--- src/LabProject3Task5.java
+++ src/LabProject3Task5.java
@@ -1,43 +1,30 @@
 
 public void main()
 {
-    Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Enter  a: ");
-    double a = scanner.nextDouble();
-
-    System.out.print("Enter b: ");
-    double b = scanner.nextDouble();
-
-    System.out.print("Enter  c: ");
-    double c = scanner.nextDouble();
-
-    double result = Math.pow(b, 2) - 4 * a * c;
-
-    System.out.println("The result is" + result);
-
-    if (result > 0)
+    int[] number = new int[10];
+    System.out.println("The random numbers generated are: ");
+    for (int count = 0; count < number.length; count++)
     {
-        double root1 = (-b + Math.sqrt(result)) / (2 * a);
-        double root2 = (-b - Math.sqrt(result)) / (2 * a);
-        System.out.println("Two roots are:");
-        System.out.println("X1 = " + root1);
-        System.out.println("X2 = " + root2);
+        number[count] = (int) (Math.random() * 100 + 1);
+        System.out.println(number[count]);
     }
-    else if (result == 0)
+    int min = 101;
+    int max = 0;
+
+    for (int i = 0; i < number.length; i++)
     {
-        double root = -b / (2 * a);
-        System.out.println("Two roots are:");
-        System.out.println("X1 = X2 = " + root);
+        if (number[i] < min)
+        {
+            min = number[i];
+        }
+        if (number[i] > max)
+        {
+            max = number[i];
+        }
     }
-    else
-    {
-        double realPart = -b / (2 * a);
-        double imaginaryPart = Math.sqrt(-result) / (2 * a);
-        System.out.println("Roots are:");
-        System.out.println("X1 = " + realPart + " - " + imaginaryPart + "i");
-        System.out.println("X2 = " + realPart + " + " + imaginaryPart + "i");
-    }
+    System.out.println("Max:" + max);
+    System.out.println("Min:" + min);
 
 }
 
