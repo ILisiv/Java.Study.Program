be860dcc6384ba99e187302f39b35b54f508ca0e
ILisiv <ilisiv.volkov01@gmail.com>
2025-11-06 10:56:32 +0100

    lab 5 task 5

--- src/LabProject5Task3.java
+++ src/LabProject5Task3.java
@@ -2,6 +2,26 @@ public static void main()
 {
     int[][] matrix = new int[10][10];
     int diagonalSum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        matrix[i][i] = i;
+        diagonalSum += i;
+    }
 
+    System.out.println("Matrix:");
+    for (int k = 0; k < 100; k++)
+    {
+        int i = k / 10;
+        int j = k % 10;
+
+        System.out.print(matrix[i][j] + " ");
+        if (j == 9)
+        {
+            System.out.println();
+        }
+    }
+
+    System.out.println("\nSum of diagonal elements: " + diagonalSum);
 }
+
 
