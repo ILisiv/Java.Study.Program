53d6a80c80cc6a12b8355a43a48facf2498cdaf6
ILisiv <ilisiv.volkov01@gmail.com>
2025-11-13 11:28:44 +0100

    check 6 lab

--- src/LabProject6Task1.java
+++ src/LabProject6Task1.java
@@ -1,21 +1,37 @@
         public static void main(String[] args)
     {
-        int[] number = {5, 12, 7, 9, 3, 15, 21, 8, 1, 6};
-        int counter = 0;
-        
+        int[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int Counter = 0;
+        boolean swap = true;
         System.out.println("Before sorting: ");
         System.out.println(Arrays.toString(number));
-        for(int j = 0; j > number.length; j++)
+
+        for (int i = 0; i < number.length - 1; i++)
         {
-            if(number[j] > number[j + 1])
+            swap = false;
+            Counter++;
+
+            for (int j = 0; j < number.length - 1; j++)
             {
-                int greater = number[j];
-                number[j] = number[j + 1];
-                number[j + 1] = greater;
+                Counter++;
+                if (number[j] > number[j + 1]) {
+                    Counter++;
+                    int greater = number[j];
+                    number[j] = number[j + 1];
+                    number[j + 1] = greater;
+                    swap = true;
+
+                }
+                if (!swap) {
+                    System.out.println("\nArray is already sorted after " + (i + 1) + " passes!");
+                    break;
+                }
             }
         }
 
-
+        System.out.println("\nAfter sorting: ");
+        System.out.println(Arrays.toString(number));
+        System.out.println("\nTotal comparisons: " + Counter);
     }
 
 
